48e28b7cecf2135e0439a67ec3c9d078cbed93c2
Duong Truong <91935964+duongtx1604@users.noreply.github.com>
2023-11-03 19:07:39 +0900

    first

---
+++ script.js
@@ -0,0 +1,153 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+// LECTURES;
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// //Map
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+// //Set
+// const currenciesUnique = new Set([
+//   'USD',
+//   'EUR',
+//   'GBP',
+//   'USD',
+//   'EUR',
+//   'GBP',
+//   'USD',
+//   'EUR',
+//   'GBP',
+// ]);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${key} ${value}`);
+// });
+
+////////////////////////////////////////
+
+// for (const n of movements) {
+//   if (n < 0) {
+//     console.log(`you withdrew ${Math.abs(n)}`);
+//   } else {
+//     console.log(`you deposited ${n}`);
+//   }
+// }
+// console.log(`-----------------------------`);
+// movements.forEach(function (n, index, array) {
+//   if (n < 0) {
+//     console.log(`Movement ${index + 1}: you withdrew ${Math.abs(n)}`);
+//   } else {
+//     console.log(`Movement ${index + 1}: you deposited ${n}`);
+//   }
+// });
+
+/////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr2 = [534, 65, 43, 2, 3, 5, 5, 8];
+
+// console.log(arr2.at(0));
+
+// //getting last array element
+// console.log(arr.at(-1));
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+
+// //.at for string
+// console.log('duong'.at(-1));
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// // SLICE
+// console.log(arr.slice(3));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2, 5));
+
+// // SLICE shallow copy
+// console.log(arr.slice());
+// console.log([...arr]);
+
+// // SPLICE like cut in windows
+// console.log(arr.splice(-1));
+// console.log(arr);
+
+// // REVERSE
+// console.log(arr.reverse());
+
+// // CONCAT
+// const letter = arr.concat(arr2);
+// console.log(letter);
+// console.log([...arr, ...arr2]);
+
+// // JOIN
+// console.log(letter.join('-'));
