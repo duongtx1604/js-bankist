1ea6c9da8ab56a52903cd45d0b1c309e6afa86b6
Duong Truong <91935964+duongtx1604@users.noreply.github.com>
2023-11-03 21:39:06 +0900

    add container movements

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300, 333, 1121, 3343],
   interestRate: 1.2, // %
   pin: 1111,
 };
@@ -61,16 +61,40 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-///////////////////////////////////////////////
-///////////////////////////////////////////////
-// LECTURES;
-
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+        <div class="movements__type movements__type--${type}">
+        ${i + 1} ${type}</div>
+        <div class="movements__value">${mov}</div>
+    </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+// LECTURES;
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // //Map
 // currencies.forEach(function (value, key, map) {
@@ -93,7 +117,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //   console.log(`${key} ${value}`);
 // });
 
-////////////////////////////////////////
+// //////////////////////////////////////
 
 // for (const n of movements) {
 //   if (n < 0) {
